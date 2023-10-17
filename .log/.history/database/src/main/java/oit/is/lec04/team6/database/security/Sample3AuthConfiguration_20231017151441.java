5a48dcea298a49389b93805f4491f379e727d820
e1b21046 <e1b21046@oit.ac.jp>
2023-10-17 15:20:53 +0900

    T7,T8 clear

---
+++ .log/.history/database/src/main/java/oit/is/lec04/team6/database/security/Sample3AuthConfiguration_20231017151441.java
@@ -0,0 +1,34 @@
+package oit.is.lec04.team6.database.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class Sample3AuthConfiguration {
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    //UserDetails user = User.withUsername("shinohara")
+        //.password("{bcrypt}$2y$10$OLzoRc0uwcA8i0NLr4GEvOJpi.t3gCaBtlNiau7nG2C7FqTyFXHlq").roles("USER").build();
+
+    //UserDetails admin = User.withUsername("kazuki")
+        //.password("{bcrypt}$2y$10$YLswQKiwk2RUBO3NS/uRmuKJoXygqXrDYtAg/C7C1fgsRCcoGrs2e").roles("ADMIN").build();
+
+        UserDetails user1 = User.withUsername("user1")
+        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e").roles("USER").build();
+    UserDetails user2 = User.withUsername("user2")
+        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e").roles("USER").build();
+    UserDetails admin = User.withUsername("admin")
+        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e").roles("ADMIN").build();
+
+    // 生成したユーザをImMemoryUserDetailsManagerに渡す（いくつでも良い）
+    return new InMemoryUserDetailsManager(user1, user2, admin);
+  }
+}
---
+++ .log/.history/database/src/main/java/oit/is/lec04/team6/security/Sample4AuthConfiguration_20231017150552.java
@@ -0,0 +1,26 @@
+package oit.is.lec04.team6.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class Sample4AuthConfiguration {
+  @Bean
+  public InMemoryUserDetailsManager userDetailsService() {
+    UserDetails user1 = User.withUsername("shinohara")
+        .password("{bcrypt}$2y$10$OLzoRc0uwcA8i0NLr4GEvOJpi.t3gCaBtlNiau7nG2C7FqTyFXHlq").roles("USER").build();
+
+    UserDetails admin = User.withUsername("kazuki")
+        .password("{bcrypt}$2y$10$YLswQKiwk2RUBO3NS/uRmuKJoXygqXrDYtAg/C7C1fgsRCcoGrs2e").roles("ADMIN").build();
+
+    return new InMemoryUserDetailsManager(user1, admin);
+  }
+}
